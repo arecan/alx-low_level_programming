4ffb56862695be8104a4156c51888b5f99874837
arecan <nossanacera@gmail.com>
2023-07-18 11:47:13 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,20 @@
 #include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
 void print_alphabet(void)
 {
-    char letter= 'a';
+char c='a';
 
-    while (letter<= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
-    _putchar('\n');
+while (c<='z')
+	{
+	_putchar(c);
+        c++;
+
+	}
+	_putchar('\n');
+
 }
-
